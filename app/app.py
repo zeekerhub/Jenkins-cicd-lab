16150f239978e49295fcaa5aae9ef85596b1248e
zeeker <macintosh@zeekers-office.local>
2026-04-12 22:43:25 +0100

    describe what you changed

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ def home():
     <h1>Jenkins + Docker CI/CD</h1>
     <p>Build Number: {os.environ.get("BUILD_NUMBER", "local")}</p>
     <p>Running on host: {socket.gethostname()}</p>
-    <p>Version: 1.0.0/Version: 1.0.1/g''
+    <p>Version: 1.0.0</p>
     '''
 
 @app.route('/health')
