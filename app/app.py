4a97b2338006b52dd064e1ee5c4ea02818d02a0a
zeeker <macintosh@zeekers-office.local>
2026-05-14 13:51:22 +0100

    Add Prometheus metrics to Flask app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from prometheus_flask_instrumentator import Instrumentator
 import os
 import socket
 
 app = Flask(__name__)
+
+# Add Prometheus metrics endpoint
+Instrumentator().instrument(app).expose(app)
 
 @app.route('/')
 def home():
@@ -10,7 +14,7 @@ def home():
     <h1>Jenkins + Docker CI/CD</h1>
     <p>Build Number: {os.environ.get("BUILD_NUMBER", "local")}</p>
     <p>Running on host: {socket.gethostname()}</p>
-    <p>Version: 1.0.0</p>
+    <p>Version: 2.0.0</p>
     '''
 
 @app.route('/health')
